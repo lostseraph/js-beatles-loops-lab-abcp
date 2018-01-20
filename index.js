31da39635be0d186edf59db8642d3f781c4142e2
Roscoe Sabale <rsabale43@gmail.com>
2018-01-20 09:35:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,3 +14,13 @@ function johnLennonFacts(facts) {
   }
   return facts;
 }
+
+function iLoveTheBeatles(number) {
+  var array = [];
+  var i = 0;
+  do {
+    array.push("I love the Beatles!");
+    i++;
+  } while(i < 15);
+  return array;
+}
