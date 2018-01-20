aab052b5e3934d6baaf78b3e01e3369568578f46
Roscoe Sabale <rsabale43@gmail.com>
2018-01-20 09:40:21 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+function theBeatlesPlay(musicians, instruments) {
+  var array = [];
+  for (let i = 0; i < musicians.length; i++) {
+    array.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return array;
+}
+
+function johnLennonFacts(facts) {
+  var n = 0;
+  while(n < facts.length) {
+    facts[n] += "!!!";
+    n++;
+  }
+  return facts;
+}
+
+function iLoveTheBeatles(number) {
+  var array = [];
+  var i = 0;
+  do {
+    array.push('I love the Beatles!');
+    i++;
+  } while(i <= number && number < 17);
+  return array;
+}
